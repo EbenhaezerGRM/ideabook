5bdfec297b201ed427cd94ff44535f7080bd82b8
EbenhaezerGRM <ebenhaezer2004@gmail.com>
2023-06-20 13:33:15 +0700

    feed page

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,21 +1,88 @@
-import React from 'react'
-import PromptCard from './PromptCard'
+"use client"
+
+import { useState, useEffect } from "react"
+import IdeaCard from "./IdeaCard"
+
+const IdeaCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className='mt-16 idea_layout'>
+      {data.map((post) => (
+        <IdeaCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  )
+}
 
 const Feed = () => {
-    return (
-        <section className='feed'>
-        <form className='relative w-full flex-center'>
-            <input
-                type='text'
-                placeholder='Search for a tag or a username...'
-                // value={}
-                // onChange={() => {}}
-                required
-                className='search_input peer'
-                />
-        </form>
+  const [allPosts, setAllPosts] = useState([])
+  const [searchText, setSearchText] = useState("")
+  const [searchTimeout, setSearchTimeout] = useState(null)
+  const [searchedResults, setSearchedResults] = useState([])
 
-        <PromptCard/>
+  const fetchPosts = async () => {
+    const response = await fetch("/api/idea")
+    const data = await response.json()
+    setAllPosts(data)
+  }
+
+  useEffect(() => {
+    fetchPosts()
+  }, [])
+
+  const filterIdea = (searchtext) => {
+    const regex = new RegExp(searchtext, "i")
+    return allPosts.filter(
+      (item) =>
+        regex.test(item.creator.username) ||
+        regex.test(item.tag) ||
+        regex.test(item.idea)
+    )
+  }
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout)
+    setSearchText(e.target.value)
+
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterIdea(e.target.value)
+        setSearchedResults(searchResult)
+      }, 500)
+    )
+  }
+
+  const handleTagClick = (tagName) => {
+    setSearchText(tagName)
+
+    const searchResult = filterIdea(tagName)
+    setSearchedResults(searchResult)
+  }
+
+  return (
+    <section className='feed'>
+      <form className='relative w-full flex-center'>
+        <input
+          type='text'
+          placeholder='Search for a tag or a username'
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className='search_input peer'
+        />
+      </form>
+
+      {searchText ? (
+        <IdeaCardList
+          data={searchedResults}
+          handleTagClick={handleTagClick}
+        />
+      ) : (
+        <IdeaCardList data={allPosts} handleTagClick={handleTagClick} />
+      )}
     </section>
   )
 }
