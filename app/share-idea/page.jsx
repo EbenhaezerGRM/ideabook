b6322bc0a1463d1ffa3c49ba6b07ac1277c9a0ea
EbenhaezerGRM <ebenhaezer2004@gmail.com>
2023-06-20 13:29:56 +0700

    share-idea page

--- app/share-idea/page.jsx
+++ app/share-idea/page.jsx
@@ -1,12 +1,51 @@
-import React from 'react'
+'use client'
+import { useState } from "react"
+import { useSession } from "next-auth/react"
+import { useRouter } from "next/navigation";
+import Form from "@components/Form"
 
-const createIdea = () => {
+const ShareIdea = () => {
+  const router = useRouter()
+  const { data: session} = useSession()
+
+  const [submitting, setSubmitting] = useState(false)
+  const [post, setPost] = useState({ idea: "", tag: ""})
+
+  const CreateIDea = async (e) => {
+    e.preventDefault()
+    setSubmitting(true)
+
+    try{
+      const response = await fetch("api/idea/new", {
+        method: "POST",
+        body: JSON.stringify({
+          idea: post.idea,
+          userId: session?.user.id,
+          tag: post.tag
+        })
+      })
+
+      if (response.ok) {
+        router.push("/");
+      }
+
+    } catch(error){
+        console.log(error)
+        
+    } finally{
+        setSubmitting(false)
+    }
+
+  }
   return (
-    <>
-      <h1 className='head_text'>SHARE YOUR IDEA</h1>
-      <p className='desc'>Share your idea here!</p>
-    </>
+    <Form
+    type='Create'
+    post={post}
+    setPost={setPost}
+    submitting={submitting}
+    handleSubmit={CreateIDea}
+    />
   )
 }
 
-export default createIdea
+export default ShareIdea
