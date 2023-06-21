a5a9fb75a0a9ed9621fa6452f63d49efc63713e0
EbenhaezerGRM <ebenhaezer2004@gmail.com>
2023-06-21 13:13:35 +0700

    add loved page

--- middleware.js
+++ middleware.js
@@ -1,3 +1,3 @@
 export { default } from "next-auth/middleware"
 
-export const config = { matcher: ["/share-idea", "/update-idea", "/profile"] }
+export const config = { matcher: ["/share-idea", "/update-idea", "/profile", "/loved"] }
