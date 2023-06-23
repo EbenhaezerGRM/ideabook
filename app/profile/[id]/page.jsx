f35b712073a1f5f0e136e98d0fdb83cab6b529e3
EbenhaezerGRM <ebenhaezer2004@gmail.com>
2023-06-23 21:20:18 +0700

    userId profile page

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+"use client"
+import { useEffect, useState } from "react"
+import { useSearchParams } from "next/navigation"
+import Profile from "@components/Profile"
 
-const userProfile = () => {
+const userProfile = ({params}) => {
+  const searchParams = useSearchParams()
+  const userName = searchParams.get("name")
+
+  const [userPosts, setUserPosts] = useState([])
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${params?.id}/posts`)
+      const data = await response.json()
+      setUserPosts(data)
+    }
+    if (params?.id) fetchPosts()
+  }, [params.id])
+
+  const formattedName = userName.endsWith('s') ? `${userName}\'` : `${userName}\'s`
+
   return (
-    <div>userProfileByID</div>
+    <Profile
+      name={userName}
+      desc={`Welcome to ${formattedName} profile page. Explore ${formattedName} idea(s) and be inspired by them`}
+      data={userPosts}
+    />
   )
 }
 
