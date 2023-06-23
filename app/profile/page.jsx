34f041dae36c0fa2c0fba9699b609df223449f3b
EbenhaezerGRM <ebenhaezer2004@gmail.com>
2023-06-23 21:20:01 +0700

    my profile page

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,11 +1,58 @@
-import React from 'react'
+"use client"
+import { useState, useEffect } from "react"
+import { useSession } from "next-auth/react"
+import { useRouter } from "next/navigation"
+import Profile from "@components/profile"
 
-const profile = () => {
+const myProfile = () => {
+
+  const router = useRouter()
+  const { data: session } = useSession()
+  const [posts, setPosts] = useState([])
+
+  useEffect(()=>{
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`)
+      const data = await response.json()
+
+      setPosts(data)
+    }
+
+    if(session?.user.id) fetchPosts()
+  }, [])
+
+  const handleEdit = (post) => {
+        router.push(`/update-idea?id=${post._id}`)
+  }
+
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm("Are you sure you want to delete this Idea?")
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/idea/${post._id.toString()}`, {
+          method: "DELETE",
+        })
+
+        const filteredPosts = posts.filter((p) => p._id !== post._id)
+        setPosts(filteredPosts)
+
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
+
   return (
-    <div>
-      My Profile
-    </div>
+    <Profile
+      name="My"
+      desc="Welcome to your personalized profile page"
+      data={posts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+      />
+
   )
 }
 
-export default profile
+export default myProfile
