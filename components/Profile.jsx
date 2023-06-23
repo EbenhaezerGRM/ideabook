6527d395c2f2e318bbf9894ac8285ee9491e02b2
EbenhaezerGRM <ebenhaezer2004@gmail.com>
2023-06-23 21:24:07 +0700

    ideaCard in my profile

---
+++ components/Profile.jsx
@@ -0,0 +1,29 @@
+import IdeaCard from "./IdeaCard"
+
+const Profile = ({name, desc, data, handleEdit, handleDelete}) => {
+  return (
+    <section className="w-full">
+      <h1 className="head_text text-left">
+        <span className="head_text">
+        {name} Profile
+        </span>
+      </h1>
+      <p className="desc text-left">
+        {desc}
+      </p>
+
+      <div className="mt-10 idea_layout">
+        {data.map((post) => 
+          <IdeaCard
+            key={post._id}
+            post={post}
+            handleEdit={() => handleEdit && handleEdit(post)}
+            handleDelete={() => handleDelete && handleDelete(post)}
+          />
+        )}
+      </div>
+    </section>
+  )
+}
+
+export default Profile
