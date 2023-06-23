907f4678fd69602751877b80afe24b6fad1924d4
EbenhaezerGRM <ebenhaezer2004@gmail.com>
2023-06-23 21:23:11 +0700

    remove unnecessary functions

--- components/IdeaCard.jsx
+++ components/IdeaCard.jsx
@@ -22,19 +22,16 @@ const IdeaCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
   const handleLoved = () => {
     setLoved(!love)
   }
-
+  
   const handleProfileClick = () => {
-    console.log(post)
-
-    if (post.creator._id === session?.user.id) return router.push("/profile");
-
-    router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
+    if (post.creator._id === session?.user.id) return router.push("/profile")
+    router.push(`/profile/${post.creator._id}?name=${post.creator.username}`)
   }
 
   return (
     <div className="idea_card">
       <div className="flex justify-between items-start gap-5">
-        <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer" onClick={handleProfileClick}>
+      <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer" onClick={handleProfileClick}>
           <Image
             src={post.creator.image}
             alt="user_image"
@@ -88,10 +85,10 @@ const IdeaCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
       </p>
             {session?.user.id === post.creator._id && pathName === '/profile' && (
               <div className="mt-5 flex-center gap-4 border-t border-gray-400 pt-3">
-                <p className="font-inter text-sm green_gradient cursor-pointer" onClick={handleEdit}>
+                <p className="font-inter text-sm text-gray-900 cursor-pointer" onClick={handleEdit}>
                   Edit
                 </p>
-                <p className="font-inter text-sm orange_gradient cursor-pointer" onClick={handleDelete}>
+                <p className="font-inter text-sm text-gray-900 cursor-pointer" onClick={handleDelete}>
                   Delete
                 </p>
               </div>
