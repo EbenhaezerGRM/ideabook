3b8cbc7ac3c5b5cba24914dd64d88f24adbadc0c
EbenhaezerGRM <ebenhaezer2004@gmail.com>
2023-07-03 14:38:19 +0700

    fix typo

--- utils/database.js
+++ utils/database.js
@@ -1,27 +1,26 @@
-import mongoose from "mongoose"
+import mongoose from 'mongoose';
 
-let isConnectedToDB = false
+let isConnected = false; // track the connection
 
 export const connectToDB = async () => {
-    mongoose.set('strictQuery', true)
+  mongoose.set('strictQuery', true);
 
-    if(isConnectedToDB){
-        console.log("MongoDB is already connected")
-        return
-    }
-    
-    try{
-        await mongoose.connect(process.env.MONGODB_URI, {
-            dbName: process.env.DB,
-            useNewUrlParser: true,
-            useUnifiedTopology: true
+  if(isConnected) {
+    console.log('MongoDB is already connected');
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "ideabook",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     })
-        
-    isConnectedToDB = true
-        console.log("MongoDB is connected")
-        
-    }catch(error){
-        console.log(error)
-    }
 
+    isConnected = true;
+
+    console.log('MongoDB connected')
+  } catch (error) {
+    console.log(error);
+  }
 }
